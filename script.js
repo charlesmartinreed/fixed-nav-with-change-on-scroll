1c51b5c9c3460236ac50c155008227e9fa13563a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-20 17:19:14 -0600

    Add styling to navbar element.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const navbarTouchEl = document.querySelector(".navbar-touch-element");
+const navbarEl = document.querySelector(".navbar-link-container");
 
 navbarTouchEl.addEventListener("click", function() {
   this.classList.toggle("navbar-open");
+  navbarEl.classList.toggle("navbar-visible");
 });
