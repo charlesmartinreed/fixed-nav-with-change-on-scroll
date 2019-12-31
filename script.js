5ce55035b9f198ba1385b9b494c57522dfd4dd83
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-31 16:14:38 -0600

    Add logo and footer element to page.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const navbarTouchEl = document.querySelector(".navbar-touch-element");
 const navbarEl = document.querySelector(".navbar-link-container");
+const logoEl = document.querySelector(".logo-container");
 
 navbarTouchEl.addEventListener("click", function() {
   this.classList.toggle("navbar-open");
   navbarEl.classList.toggle("navbar-visible");
+  logoEl.classList.toggle("logo-not-visible");
 });
