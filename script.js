77dc7d256b08ba90200bc4413ea332a57bc93bd6
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-31 19:33:10 -0600

    Add toggle feature for show/hide of extra blog posts.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const navbarTouchEl = document.querySelector(".navbar-touch-element");
 const navbarEl = document.querySelector(".navbar-link-container");
 const logoEl = document.querySelector(".logo-container");
+const blogPermalink = document.querySelectorAll(".blog-permalink");
+const blogContents = document.querySelectorAll(".blog-contents");
+
+let fullBlogVisible = false;
 
 navbarTouchEl.addEventListener("click", function() {
   this.classList.toggle("navbar-open");
   navbarEl.classList.toggle("navbar-visible");
   logoEl.classList.toggle("logo-not-visible");
+});
+
+blogPermalink.forEach(link => {
+  link.addEventListener("click", e => {
+    if (!fullBlogVisible) {
+      e.preventDefault();
+    }
+
+    link.classList.toggle("full-blog-active");
+    link.textContent = fullBlogVisible ? "Continue reading" : "Finish reading";
+
+    // e.preventDefault();
+    fullBlogVisible = !fullBlogVisible;
+
+    link.previousElementSibling.classList.toggle("full-blog-visible");
+  });
 });
