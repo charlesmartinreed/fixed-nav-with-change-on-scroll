569620f1f009f5a23dc9ee2b4c42b7a273e3b5ed
charlesmartinreed <charlesmartinreed@icloud.com>
2020-05-16 20:31:51 -0500

    Add 'How Soon is Then' project.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const projects = [
     projectRepo:
       "https://github.com/charlesmartinreed/everything-is-changing-colour",
     projectDescription:
-      "A UI/UX mockup of a progressive web app intended to tie into a NoSQL backend for allowing users to stream their uploaded music from the cloud."
+      "A UI/UX mockup of a progressive web app intended to tie into a NoSQL backend for allowing users to stream their uploaded music from the cloud.",
   },
   {
     projectID: "1",
@@ -15,7 +15,7 @@ const projects = [
     projectRepo:
       "https://github.com/charlesmartinreed/yet-another-password-generator",
     projectDescription:
-      "A simple, clean password generator that I designed to help my grandfather come up with safe, secure passwords that he could confidently use to cover his digital footprints. Copy password on click implemented with a bit of JavaScript."
+      "A simple, clean password generator that I designed to help my grandfather come up with safe, secure passwords that he could confidently use to cover his digital footprints. Copy password on click implemented with a bit of JavaScript.",
   },
   // {
   //   projectID: "2",
@@ -27,13 +27,12 @@ const projects = [
   //     "A minimalist website designed for a young, yet undiscovered novelist featuring just the written word and some words written about the author. Integrates with Stripe."
   // },
   {
-    projectID: "2",
-    projectName: "Shop Meet",
-    projectURL: "./portfolio-projects/project-4/index.html",
-    projectRepo:
-      "https://github.com/charlesmartinreed/mobile-first-shopping-layout",
+    projectID: "3",
+    projectName: "How Soon Is Then?",
+    projectURL: "https://how-soon-is-now.netlify.app/",
+    projectRepo: "https://github.com/charlesmartinreed/how-soon-is-then",
     projectDescription:
-      "How would you like to go shopping with someone who could dress you and undress you with equal skill? Try Shop Meet, a dating app where you shop until you drop (your pants)."
+      "A basic, single place React app using functional components and hooks (useState, useEffect, etc....)",
   },
   {
     projectID: "3",
@@ -41,7 +40,7 @@ const projects = [
     projectURL: "./portfolio-projects/project-7/index.html",
     projectRepo: "https://github.com/charlesmartinreed/typing-test-demo-app",
     projectDescription:
-      "Created in a few hours as a design challenge and because Mavis Beacon remains forever in my heart. Uses just pure HTML/CSS and JavaScript."
+      "Created in a few hours as a design challenge and because Mavis Beacon remains forever in my heart. Uses just pure HTML/CSS and JavaScript.",
   },
   {
     projectID: "4",
@@ -50,7 +49,7 @@ const projects = [
     projectRepo:
       "https://github.com/charlesmartinreed/animated-navigation-example",
     projectDescription:
-      "Design prospect for a restaurant website. Simple, clean CSS and some light vanilla JavaScript for animations. Gave me a chance to play around with Intersection Observers."
+      "Design prospect for a restaurant website. Simple, clean CSS and some light vanilla JavaScript for animations. Gave me a chance to play around with Intersection Observers.",
   },
   {
     projectID: "5",
@@ -58,8 +57,8 @@ const projects = [
     projectURL: "./portfolio-projects/project-6/index.html",
     projectRepo: "https://github.com/charlesmartinreed/what-time-is-now",
     projectDescription:
-      "Written as a design challenge to create a clean, modern looking 'analog' clock. Each day comes with a bit of sassy text just to make you smile."
-  }
+      "Written as a design challenge to create a clean, modern looking 'analog' clock. Each day comes with a bit of sassy text just to make you smile.",
+  },
 ];
 
 // EVENT LISTENER FOR ARROW ELEMENTS
@@ -69,7 +68,7 @@ const nextProjectArrow = document.querySelector(".arrow-right");
 
 const projectArrowElems = document.querySelectorAll(".arrow");
 
-projectArrowElems.forEach(arrow => {
+projectArrowElems.forEach((arrow) => {
   arrow.addEventListener("click", () => {
     // console.log(arrow.dataset.direction);
     if (arrow.dataset.direction === "left") {
@@ -92,7 +91,7 @@ projectArrowElems.forEach(arrow => {
   });
 });
 
-const displayProject = projectIdx => {
+const displayProject = (projectIdx) => {
   // grab the necessary elements
   let project = projects[projectIdx];
 
@@ -110,15 +109,15 @@ const displayProject = projectIdx => {
 const navbarTouchEl = document.querySelector(".navbar-touch-element");
 const navbarSquares = document.querySelectorAll(".navbar-square");
 
-const navbar = document.querySelector('.navbar')
+const navbar = document.querySelector(".navbar");
 const navbarEl = document.querySelector(".navbar-link-container");
 const navLinks = document.querySelectorAll(".navbar-link-container a");
 const logoEl = document.querySelector(".logo-container");
 const blogPermalink = document.querySelectorAll(".blog-permalink");
 const blogContents = document.querySelectorAll(".blog-contents");
-const blogTitles = document.querySelectorAll('.blog-title');
+const blogTitles = document.querySelectorAll(".blog-title");
 
-const jumpToTopEl = document.getElementById('to-top-icon');
+const jumpToTopEl = document.getElementById("to-top-icon");
 
 let currentProjectIdx = 0;
 
@@ -127,15 +126,15 @@ let fullBlogVisible = false;
 // TODO: add active class to clicked nav links
 
 // CLICK ACTION FOR NAVBAR LINK CONTAINER/LOGO
-navbarTouchEl.addEventListener("click", function() {
+navbarTouchEl.addEventListener("click", function () {
   this.classList.toggle("navbar-open");
-  navbar.classList.toggle('navbar-visible');
+  navbar.classList.toggle("navbar-visible");
   navbarEl.classList.toggle("navbar-visible");
   logoEl.classList.toggle("logo-not-visible");
 });
 
-blogPermalink.forEach(link => {
-  link.addEventListener("click", e => {
+blogPermalink.forEach((link) => {
+  link.addEventListener("click", (e) => {
     if (!fullBlogVisible) {
       e.preventDefault();
     }
@@ -155,23 +154,23 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 // OBSERVER FOR TO TOP ARROW
-const footerCheck = entries => {
-  entries.forEach(entry => {
+const footerCheck = (entries) => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      console.log('intersecting now')
-      jumpToTopEl.classList.add('active')
+      console.log("intersecting now");
+      jumpToTopEl.classList.add("active");
     } else {
-      console.log('out of intersection range')
-      jumpToTopEl.classList.remove('active')
+      console.log("out of intersection range");
+      jumpToTopEl.classList.remove("active");
     }
-  })
-}
+  });
+};
 
 let observer = new IntersectionObserver(footerCheck, {
-  threshold: [0.5]
-})
+  threshold: [0.5],
+});
 
-observer.observe(document.getElementById('page-footer'))
+observer.observe(document.getElementById("page-footer"));
 
 // blogTitles.forEach(title => {
 //   observer.observe(title)
